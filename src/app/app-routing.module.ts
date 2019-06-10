104eaccf9410996d0a3feea15a51a82f7b314783
Mick Evans <codemastermick@gmail.com>
2019-06-10 16:18:35 -0400

    chore: run angular migration scripts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,14 +27,14 @@ const routes: Routes = [
   {
     path: "404",
     loadChildren:
-      "../app/pages/errors/not-found/not-found.module#NotFoundModule"
+      () => import('../app/pages/errors/not-found/not-found.module').then(m => m.NotFoundModule)
   },
   { path: "", redirectTo: "/home", pathMatch: "full" }, // Fallback to home if no route is found
   {
     path: "**",
     resolve: { path: PathResolveService },
     loadChildren:
-      "../app/pages/errors/not-found/not-found.module#NotFoundModule"
+      () => import('../app/pages/errors/not-found/not-found.module').then(m => m.NotFoundModule)
   }
 ];
 
--- src/app/shared/path-resolve-service.guard.ts
+++ src/app/shared/path-resolve-service.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
 import { paths } from "../app-paths";
 
 @Injectable({
