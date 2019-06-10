2dfd65c1fcbf73e194e5e9b1fd6309b118807f51
Mick Evans <codemastermick@gmail.com>
2019-06-10 04:52:08 -0400

    feat: add ssr support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
