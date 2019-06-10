b547a6d572b6afa50ab54d35bf91804b6931c0ca
Mick Evans <codemastermick@gmail.com>
2019-06-10 16:14:00 -0400

    chore: prettify code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from "@angular/core";
-
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = "ngFAST";           // replace this with your app"s title
-  foundingYear = 2019;        // replace with this year
-  author = "Your name here";  // your name or your company name
-
+  title = "ngFAST"; // replace this with your app"s title
+  foundingYear = 2019; // replace with this year
+  author = "Your name here"; // your name or your company name
 }
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from "@angular/core";
 import { ServerModule } from "@angular/platform-server";
-
 import { AppModule } from "./app.module";
 import { AppComponent } from "./app.component";
 import { Routes, RouterModule } from "@angular/router";
 import { AppShellComponent } from "./app-shell/app-shell.component";
 
-const routes: Routes = [ { path: "home", component: AppShellComponent }];
+const routes: Routes = [{ path: "home", component: AppShellComponent }];
 
 @NgModule({
-  imports: [
-    AppModule,
-    ServerModule,
-    RouterModule.forRoot(routes),
-  ],
+  imports: [AppModule, ServerModule, RouterModule.forRoot(routes)],
   bootstrap: [AppComponent],
-  declarations: [AppShellComponent],
+  declarations: [AppShellComponent]
 })
 export class AppServerModule {}
