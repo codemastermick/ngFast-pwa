265a330b64656688cfeba155e71008cd07017e00
Mick Evans <codemastermick@gmail.com>
2019-06-10 09:45:41 -0400

    feat: fill in routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { paths } from "./app-paths";
+import { PathResolveService } from "./shared/path-resolve-service.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: paths.home,
+    loadChildren: () =>
+      import("./pages/home/home.module").then(m => m.HomeModule)
+  },
+  {
+    path: paths.about,
+    loadChildren: () =>
+      import("./pages/about/about.module").then(m => m.AboutModule)
+  },
+  {
+    path: paths.contact,
+    loadChildren: () =>
+      import("./pages/contact/contact.module").then(m => m.ContactModule)
+  },
+  {
+    path: paths.docs,
+    loadChildren: () =>
+      import("./pages/docs/docs.module").then(m => m.DocsModule)
+  },
+  {
+    path: "404",
+    loadChildren:
+      "../app/pages/errors/not-found/not-found.module#NotFoundModule"
+  },
+  { path: "", redirectTo: "/home", pathMatch: "full" }, // Fallback to home if no route is found
+  {
+    path: "**",
+    resolve: { path: PathResolveService },
+    loadChildren:
+      "../app/pages/errors/not-found/not-found.module#NotFoundModule"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
